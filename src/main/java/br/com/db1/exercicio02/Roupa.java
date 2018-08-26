12ae73003bc39ed1b75919023e9f4aed951c8d5b
João Lacerda Nascimento <joao.lacerda0@gmail.com>
2018-08-26 08:33:00 -0300

    Exercicio05

--- src/main/java/br/com/db1/exercicio02/Roupa.java
+++ src/main/java/br/com/db1/exercicio02/Roupa.java
@@ -1,5 +1,21 @@
 package br.com.db1.exercicio02;
 
 public class Roupa {
-
+	private String marca;
+	private Integer anoFabricao;
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public Integer getAnoFabricao() {
+		return anoFabricao;
+	}
+	public void setAnoFabricao(Integer anoFabricao) {
+		this.anoFabricao = anoFabricao;
+	}
+	
+	
 }
---
+++ src/main/java/br/com/db1/exercicio05/Barco.java
@@ -0,0 +1,18 @@
+package br.com.db1.exercicio05;
+
+public class Barco extends TransporteAquatico{
+	private Boolean pesqueiro;
+
+	public Boolean isPesqueiro(){
+		return Boolean.TRUE;
+	}
+	
+	public Boolean getPesqueiro() {
+		return pesqueiro;
+	}
+
+	public void setPesqueiro(Boolean pesqueiro) {
+		this.pesqueiro = pesqueiro;
+	}
+	
+}
