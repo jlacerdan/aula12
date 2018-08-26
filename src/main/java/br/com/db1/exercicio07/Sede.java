7bf342137cb88ef4e676600f6ada055464d0576e
João Lacerda Nascimento <joao.lacerda0@gmail.com>
2018-08-26 09:35:50 -0300

    Exercicio07

---
+++ src/main/java/br/com/db1/exercicio07/Sede.java
@@ -0,0 +1,23 @@
+package br.com.db1.exercicio07;
+
+public class Sede {
+	private String nomeCidade;
+	private Pais pais;
+	
+	public Pais getPais() {
+		return pais;
+	}
+
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+
+	public String getNomeCidade() {
+		return nomeCidade;
+	}
+
+	public void setNomeCidade(String nomeCidade) {
+		this.nomeCidade = nomeCidade;
+	}
+	
+}
