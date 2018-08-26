2e1c35ec9f08eb9413bb05c558b7dcb215c6e312
João Lacerda Nascimento <joao.lacerda0@gmail.com>
2018-08-26 08:19:27 -0300

    Exercicio03

--- src/main/java/br/com/db1/exercicio03/Calculadora.java
+++ src/main/java/br/com/db1/exercicio03/Calculadora.java
@@ -1,5 +1,61 @@
 package br.com.db1.exercicio03;
 
 public class Calculadora {
+	private Double resultado;
+	private Double primeiroValor;
+	private Double segundoValor;
+
+	public void zerarCalculo() {
+
+	}
+
+	public void somar(Double valor1, Double valor2) {
+
+	}
+
+	public void subtrair(Double valor1, Double valor2) {
+
+	}
+
+	public void dividir(Double valor1, Double valor2) {
+
+	}
+
+	public void multiplicar(Double valor1, Double valor2) {
+
+	}
+
+	public Double exibirResultado(){
+		return resultado;
+	}
+	
+	public Calculadora(Double valor1, Double valor2){
+		primeiroValor = valor1;
+		segundoValor = valor2;
+	}
+	
+	public Double getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(Double resultado) {
+		this.resultado = resultado;
+	}
+
+	public Double getPrimeiroValor() {
+		return primeiroValor;
+	}
+
+	public void setPrimeiroValor(Double primeiroValor) {
+		this.primeiroValor = primeiroValor;
+	}
+
+	public Double getSegundoValor() {
+		return segundoValor;
+	}
+
+	public void setSegundoValor(Double segundoValor) {
+		this.segundoValor = segundoValor;
+	}
 
 }
